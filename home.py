e42b9a30a6dcae95ddb08cc25ab425ef2dd8e414
kevinfitzpatrick <mastery414@gmail.com>
2016-11-30 16:28:14 +0000

    This is my Py3 change

--- home.py
+++ home.py
@@ -1,9 +1,16 @@
+
 from flask import Flask
 app=Flask(__name__)
 
+
+
 @app.route('/')
 def home():
-    return  "This is the first change!!!"
+    random = []
+    dicty = {'hello':44, 'goodbye':77}
+    for key, item in dicty.items():
+        random.append(item)
+    return random
 
 if __name__ == "__main__":
     app.run()
