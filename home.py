09452d4548cc84bbb38c28679e130fa157aab948
kevinfitzpatrick <mastery414@gmail.com>
2016-12-01 18:33:44 +0000

    Wont work again

--- home.py
+++ home.py
@@ -7,34 +7,34 @@ app= Flask(__name__)
 
 DATABASE = '/var/www/html/FlaskApps/CalendarApp/calendar.db'
 
-app.config.from_object(__name__)
+# app.config.from_object(__name__)
 
-def connect_to_database():
-    return sqlite3.connect(app.config['DATABASE'])
-
-def get_db():
-    db = getattr(g, 'db', None)
-    if db is None:
-        db = g.db = connect_to_database()
-    return db
-
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, 'db', None)
-    if db is not None:
-        db.close()
-
-def execute_query(query, args=()):
-    cur = get_db().execute(query, args)
-    rows = cur.fetchall()
-    cur.close()
-    return rows
+# def connect_to_database():
+#     return sqlite3.connect(app.config['DATABASE'])
+#
+# def get_db():
+#     db = getattr(g, 'db', None)
+#     if db is None:
+#         db = g.db = connect_to_database()
+#     return db
+#
+# @app.teardown_appcontext
+# def close_connection(exception):
+#     db = getattr(g, 'db', None)
+#     if db is not None:
+#         db.close()
+#
+# def execute_query(query, args=()):
+#     cur = get_db().execute(query, args)
+#     rows = cur.fetchall()
+#     cur.close()
+#     return rows
 
 
 @app.route('/')
 def home():
-    # conn = sqlite3.connect('calendar.db')
-    # c = conn.cursor()
+    conn = sqlite3.connect(DATABASE)
+    c = conn.cursor()
     # command = "SELECT * FROM calendarevents"
     # c.execute(command)
     # results = c.fetchall()
