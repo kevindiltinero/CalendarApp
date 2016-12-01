2d88c491bb7570ff8d49c55d47cc64fe37115087
kevinfitzpatrick <mastery414@gmail.com>
2016-12-01 19:03:22 +0000

    Wont work again

--- home.py
+++ home.py
@@ -1,32 +1,47 @@
-from flask import Flask, g
 import sqlite3
 
+from flask import Flask, request, g
+
+DATABASE = '/var/www/html/FlaskApps/CalendarApp/calendar.db'
+
 app = Flask(__name__)
-DATABASE = './dublinbikes_3.db'
+app.config.from_object(__name__)
 
-#Connect to database
 def connect_to_database():
-    return sqlite3.connect(DATABASE)
+    return sqlite3.connect(app.config['DATABASE'])
 
-#Get the database
 def get_db():
-    db = getattr(g, '_database',	None)
+    db = getattr(g, 'db', None)
     if db is None:
-        db = g._database = connect_to_database()
+        db = g.db = connect_to_database()
     return db
-
 
 @app.teardown_appcontext
 def close_connection(exception):
-    db = getattr(g, '_database',	None)
+    db = getattr(g, 'db', None)
     if db is not None:
         db.close()
 
-@app.route('/')
-def index():
-    conn = get_db()
-    return "Jesus"
+def execute_query(query, args=()):
+    cur = get_db().execute(query, args)
+    rows = cur.fetchall()
+    cur.close()
+    return rows
+#
+# @app.route("/viewdb")
+# def viewdb():
+#     rows = execute_query("""SELECT * FROM natlpark""")
+#     return '<br>'.join(str(row) for row in rows)
+#
+# @app.route("/state/<state>")
+# def state_query(state):
+#     rows = execute_query("""SELECT * FROM natlpark WHERE state = ?""",
+#                          [state.title()])
+#     return '<br>'.join(str(row) for row in rows)
 
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
