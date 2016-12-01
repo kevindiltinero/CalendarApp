b707d22335f260578fca99e98c5fffcec00665de
kevinfitzpatrick <mastery414@gmail.com>
2016-12-01 18:29:33 +0000

    Wont work

--- home.py
+++ home.py
@@ -1,16 +1,40 @@
-from flask import Flask,url_for, render_template, request
+from flask import Flask,url_for, render_template, request, g
 import sqlite3
 import json
 
 
 app= Flask(__name__)
 
+DATABASE = '/var/www/html/FlaskApps/CalendarApp/calendar.db'
+
+app.config.from_object(__name__)
+
+def connect_to_database():
+    return sqlite3.connect(app.config['DATABASE'])
+
+def get_db():
+    db = getattr(g, 'db', None)
+    if db is None:
+        db = g.db = connect_to_database()
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
+def execute_query(query, args=()):
+    cur = get_db().execute(query, args)
+    rows = cur.fetchall()
+    cur.close()
+    return rows
 
 
 @app.route('/')
 def home():
-    conn = sqlite3.connect('calendar.db')
-    c = conn.cursor()
+    # conn = sqlite3.connect('calendar.db')
+    # c = conn.cursor()
     # command = "SELECT * FROM calendarevents"
     # c.execute(command)
     # results = c.fetchall()
@@ -18,6 +42,11 @@ def home():
     results = "goodbye"
 
     return results
+
+@app.route("/viewdb")
+def viewdb():
+    rows = execute_query("""SELECT * FROM calendarevents""")
+    return '<br>'.join(str(row) for row in rows)
 
 if __name__ == "__main__":
     app.run()
